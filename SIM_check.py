c8c2334215e2d9d7461a5f7373f85dbbd6a6485e
Nava Tavasoli <navatavasoli8@gmail.com>
2024-10-24 10:55:31 -0400

    Create SIM_check.py

---
+++ SIM_check.py
@@ -0,0 +1,20 @@
+# draft 1 - legitimate detection vs illegitimate 
+
+# json // csv file(s) 
+
+#loop function to continuously read the data
+# SIM cards currently registered in this enterprise's bandwidth 
+# write a function to obtain the SIM of the device from settings OR just have a list and parse the bandwidth data of registered SIMs in the data 
+
+# import the JSON file, parse the JSON file loop once every hour 
+
+
+
+import json
+
+JSON sim_data = {SIM: '89900123450004598765'}
+
+python_obj = json.loads(json_data)
+
+
+# get a file (CSV or JSON) with data on the active SIM cards in the system currently 
