12e5e4294570c22c934d9b2f6f60d4a087510479
notnycee <cocogaming23@gmail.com>
2024-11-08 20:35:45 -0500

    Update AANNG_user_interface.py
    I've added colour to the user interface, shadobi icons, and a welcome image!!

--- AANNG_user_interface.py
+++ AANNG_user_interface.py
@@ -1,9 +1,29 @@
 import tkinter as tk
+from PIL import Image, ImageTk
+
+# window configuration and greeting label
 window = tk.Tk()
-window.configure(bg="white") #maroon colour 
-window.geometry("400x400")
-greeting = tk.Label(text="USER INTERFACE PROTOTYPE FOR AANNG")
-greeting.pack()
+window.configure(bg="#060129") #shabodi colour background
+window.geometry("500x500")
+
+# creating the Shabodi greeting image 
+greeting_image_path = 'C:/Users/Neyssa/Downloads/shabodi_icon.jpg'
+greeting_image = Image.open(greeting_image_path)
+resized_image = greeting_image.resize((50, 50))  # Resize to 50x50 pixels
+photo = ImageTk.PhotoImage(resized_image)
+
+greeting = tk.Label(text=" Welcome To AANNG Sim Swap Fraud Alert Application!", background='#060129', foreground='white', image=photo, compound='left')
+greeting2 = tk.Label(text="How Would You Like To Proceed?", background='#060129', foreground='white')
+greeting.pack(pady=2)
+greeting2.pack(pady=1)
+greeting.image = photo
+
+# shabodi icon on the window
+path = "C:/Users/Neyssa/Downloads/shabodi_icon.jpg"
+load = Image.open(path)
+render = ImageTk.PhotoImage(load)
+window.iconphoto(False, render)
+
 
 #new window for monitoring sim card activity
 def open_new_window():
@@ -17,7 +37,7 @@ def open_new_window():
 # new window for sim card history
 def open_new_window2():
     new_window2 = tk.Toplevel(window)
-    new_window2.title("Sim Card History")
+    new_window2.title("Review Sim Card History")
     new_window2.geometry("300x200")
     tk.Label(new_window2, text="Sim Card History").pack(pady=20)
     tk.Label(new_window2, text="here past alerts or locations, information of that sort should be saved here")
@@ -34,9 +54,9 @@ def on_button_click(button_number):
     print("redirecting to {button_number} window ")
 
 buttons = [
-    tk.Button(window, text="Monitor Sim Card Activity", command=open_new_window, height=2, width=20),
-    tk.Button(window, text="Sim Card History", command=open_new_window2, height=2, width=20),
-    tk.Button(window, text="Settings", command=open_new_window3, height=2, width=20),
+    tk.Button(window, text="Monitor Sim Card Activity", command=open_new_window, height=2, width=20, background='#0948AD', foreground='white'),
+    tk.Button(window, text="Review Sim Card History", command=open_new_window2, height=2, width=20, background='#0948AD', foreground='white'),
+    tk.Button(window, text="Settings", command=open_new_window3, height=2, width=20, background='#0948AD', foreground='white'),
 ]
 
 # Center the buttons for the user interface
@@ -51,3 +71,5 @@ button.pack(expand=True)
 
 
 window.mainloop() # need a mainloop so that the window actually pops up and stays up or else if you dont have that, you won't see anything
+
+# go into that json file, we dont wnat this card, ability to swap sim that way. take nava's code and plop it into the user history. 
